3e635b898181eb27a967f6c3cd4362feb0005fcd
rahul-bash <rahul-dev@bluebash.co>
2023-09-04 15:53:58 +0530

    Stable 4.2

--- app/models/spree_admin_roles_and_access/spree/user_decorator.rb
+++ app/models/spree_admin_roles_and_access/spree/user_decorator.rb
@@ -4,6 +4,12 @@ module SpreeAdminRolesAndAccess
       def self.prepended(base)
         base.alias_attribute :roles, :spree_roles
       end
+
+      def can_access_admin_dashboard?
+        return false unless roles
+
+        roles.pluck(:admin_accessible).include?(true)
+      end
     end
   end
 end
